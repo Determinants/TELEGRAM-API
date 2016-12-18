32941704ffa649a64b146bc1a6ec0ff00baadf37
otipella <yaw.oti-boateng@st.vvu.edu.gh>
2016-12-18 07:12:05 -1200

    Create index.php

---
+++ index.php
@@ -0,0 +1,103 @@
+//Codes for VVU INFO BOT 
+
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>VVU INFO BOT</title>
+   <link rel="stylesheet" type="text/css" href="formValidators/vendor/bootstrap/css/bootstrap-theme.css"
+/>
+    <link rel="stylesheet" type="text/css" href="formValidators/vendor/bootstrap/css/bootstrap.css"/>
+    <script type="text/javascript" href="formValidators/vendor/bootstrap/js/bootstrap.js"></script>
+    <script type="text/javascript" href="formValidators/vendor/jquery/jquery-1.10.2.min.js"></script>
+    <link rel="stylesheet" href="api.css" type="text/css">
+
+    
+    <!-- Start WOWSlider.com HEAD section -->
+<link rel="stylesheet" type="text/css" href="engine1/style.css" />
+<script type="text/javascript" src="engine1/jquery.js"></script>
+<!-- End WOWSlider.com HEAD section -->
+</head>
+<body>
+    <header>
+        <nav id="nbar" class="navbar navbar-defaullt navbar-inverse navbar-fixed-top" role="navigation">
+    
+    <div class="container">
+    <div class="navbar-header">
+    <nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">VVU INFO BOT</a>
+    </div>
+        <ul class="nav navbar-nav">
+            <li class="active"><a href="index.php"><span  class="glyphicon glyphicon-home">Home </span></a></li>
+            <li class="active"><a href="deploymessages.php"> <span class="glyphicon glyphicon-share">Contact Us</span></a></li> 
+            </ul>
+      <button  type="button" class="btn btn-info btn-lg"  data-toggle="modal" data-target="#myModal"><span class="glyphicon glyphicon-envelope">Send Message</span></button>
+    </div>
+    </nav>      
+    
+    </header>
+    <!-- Start WOWSlider.com BODY section -->
+<div id="wowslider-container1">
+<div class="ws_images"><ul>
+		<li><img src="data1/images/wallpaper2251330.jpg" alt="VVU INFO BOT" title="VVU INFO BOT&" id="wows1_0"/></li>
+		<li><img src="data1/images/wallpaper2291263.jpg" alt="VVU INFO BOT" title="VVU INFO BOT" id="wows1_1"/></li>
+		<li><img src="data1/images/wallpaper2490835.jpg" alt="VVU INFO BOT" title="VVU INFO BOT" id="wows1_2"/></li>
+		<li><img src="data1/images/wallpaper2700373.jpg" alt="VVU INFO BOT" title="VVU INFO BOT" id="wows1_3"/></li>
+		<li><img src="data1/images/wallpaper2641851.png" alt="VVU INFO BOT" title="VVU INFO BOT" id="wows1_4"/></li>
+		<li><a href="http://wowslider.com"><img src="data1/images/wallpaper1013331.jpg" alt="responsive carousel" title="Abel&MillsApi" id="wows1_5"/></a></li>
+		<li><img src="data1/images/wallpaper2910790.jpg" alt="VVU INFO BOT" title="VVU INFO BOT" id="wows1_6"/></li>
+	</ul></div>
+
+	<div class="ws_bullets"><div>
+		<a href="#" title="VVU INFO BOT"><span><img src="data1/tooltips/wallpaper2251330.jpg" alt="VVU INFO BOT"/>1</span></a>
+		<a href="#" title="VVU INFO BOT"><span><img src="data1/tooltips/wallpaper2291263.jpg" alt="VVU INFO BOT"/>2</span></a>
+		<a href="#" title="VVU INFO BOT"><span><img src="data1/tooltips/wallpaper2490835.jpg" alt="VVU INFO BOT"/>3</span></a>
+		<a href="#" title="VVU INFO BOT"><span><img src="data1/tooltips/wallpaper2700373.jpg" alt="VVU INFO BOT"/>4</span></a>
+		<a href="#" title="VVU INFO BOT"><span><img src="data1/tooltips/wallpaper2641851.png" alt="VVU INFO BOT"/>5</span></a>
+		<a href="#" title="VVU INFO BOT"><span><img src="data1/tooltips/wallpaper1013331.jpg" alt="VVU INFO BOT"/>6</span></a>
+		<a href="#" title="VVU INFO BOT"><span><img src="data1/tooltips/wallpaper2910790.jpg" alt="VVU INFO BOT"/>7</span></a>
+	</div></div><div class="ws_script" style="position:absolute;left:-99%"><a href="http://wowslider.com/vi">html slider</a> by WOWSlider.com v7.8</div>
+<div class="ws_shadow"></div>
+</div>	
+<script type="text/javascript" src="engine1/wowslider.js"></script>
+<script type="text/javascript" src="engine1/script.js"></script>
+<!-- End WOWSlider.com BODY section --> 
+        
+
+    <div id="myModal" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+
+    <!-- Modal content-->
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h4 class="modal-title">Send Bot Message</h4>
+         <img src="image/telegram.png" class="img-circle" alt="Telegram" width="150" height="95" style="float:right">
+      </div>
+      <div class="modal-body">
+          <form name="sendngMessage" method="post" action="#">
+          <div class="form-group">
+              <input class="form-control" name="message" type="text">
+              
+            <button class="btn btn-success" name="send" type="submit" value="Send" ><span class="glyphicon glyphicon-envelope"></span> </button>
+              </div>
+          
+          </form>
+          
+           </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+
+  </div>
+</div>
+ </body>
+      
+
+</html>
+    
